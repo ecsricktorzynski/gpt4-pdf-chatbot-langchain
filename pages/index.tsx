828c66f117eb4f6fd33a7459e7020bc247b8e369
ecsricktorzynski <richard.torzynski@ecstech.com>
2023-04-20 14:35:59 -0400

    Make changes for resume

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        message: 'Hi, what would you like to know?',
         type: 'apiMessage',
       },
     ],
@@ -165,7 +165,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Chat With Your Documents
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -282,7 +282,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'What do you want to know?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
